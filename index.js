08a3629e8ed7bcd3b14af15232212629e097c413
Pavel Rampas <pavelrampas@gmail.com>
2026-04-07 13:44:51 +0200

    Add more textreas

--- index.js
+++ index.js
@@ -1,11 +1,20 @@
 // variables ------------------------------------------------------------------
 
-const textarea = document.getElementById('textarea');
+const textareas = [
+  document.getElementById('textarea1'),
+  document.getElementById('textarea2'),
+  document.getElementById('textarea3'),
+];
+const tabButtons = [
+  document.getElementById('tab1'),
+  document.getElementById('tab2'),
+  document.getElementById('tab3'),
+];
 const timer = document.getElementById('timer');
 const actual = document.getElementById('actual');
 const next = document.getElementById('next');
 
-const storeExercises = 'exercises';
+const storeExercises = ['exercises1', 'exercises2', 'exercises3'];
 
 const typePause = 'pause';
 const typeExercise = 'exercise';
@@ -13,6 +22,8 @@ const colorPause = 'pause-color';
 const colorExercise = 'exercise-color';
 
 const audio = new Audio('whistle.mp3');
+
+let activeTab = 0;
 
 let data = [];
 
@@ -25,26 +36,47 @@ let wakeLock = null;
 
 // code -----------------------------------------------------------------------
 
-if (localStorage.getItem(storeExercises)) {
-  textarea.value = localStorage.getItem(storeExercises);
-} else {
-  textarea.value =
-    '-příprava;10\n' + '+kliky;30\n' + '-pauza;20\n' + '+dřepy;30\n';
-}
+textareas.forEach(function (textarea, i) {
+  if (i !== activeTab) {
+    textarea.style.display = 'none';
+  }
+  if (localStorage.getItem(storeExercises[i])) {
+    textarea.value = localStorage.getItem(storeExercises[i]);
+  } else if (i === 0) {
+    textarea.value =
+      '-příprava;10\n' + '+kliky;30\n' + '-pauza;20\n' + '+dřepy;30\n';
+  }
+});
 
 // onclick functions ----------------------------------------------------------
 
 document.getElementById('save').onclick = function clickSave(e) {
-  localStorage.setItem(storeExercises, textarea.value);
+  localStorage.setItem(storeExercises[activeTab], textareas[activeTab].value);
 };
+
+tabButtons.forEach(function (button, i) {
+  button.onclick = function () {
+    switchTab(i);
+  };
+});
+
+function switchTab(i) {
+  textareas[activeTab].style.display = 'none';
+  tabButtons[activeTab].classList.remove('active');
+  activeTab = i;
+  textareas[activeTab].style.display = '';
+  tabButtons[activeTab].classList.add('active');
+}
 
 document.getElementById('start').onclick = function clickStart(e) {
   if (stoptime === true && pause === false) {
-    localStorage.setItem(storeExercises, textarea.value);
+    localStorage.setItem(storeExercises[activeTab], textareas[activeTab].value);
 
     reset();
 
-    data = parseTextarea(localStorage.getItem(storeExercises).split('\n'));
+    data = parseTextarea(
+      localStorage.getItem(storeExercises[activeTab]).split('\n')
+    );
 
     if ('wakeLock' in navigator) {
       wakeLockRequest();
